f9f009741a61651f33c622082f7864cd66caedf9
NickSingizwa <singizwanick19@gmail.com>
2022-02-25 08:21:58 +0200

    final demo

--- components/Player.js
+++ components/Player.js
@@ -1,11 +1,12 @@
 import {useSession} from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import {useRecoilState} from 'recoil'
 import {currentTrackIdState,isPlayingState} from '../atoms/songAtom';
 import useSpotify from "../hooks/useSpotify";
 import useSongInfo from "../hooks/useSongInfo"
 import { HeartIcon,VolumeUpIcon as VolumeDownIcon,MicrophoneIcon } from "@heroicons/react/outline";
 import { RewindIcon,FastForwardIcon,PauseIcon,PlayIcon,ReplyIcon,VolumeUpIcon,SwitchHorizontalIcon } from "@heroicons/react/solid";
+import {debounce} from "lodash";
 
 //debounce for handling the volume change(for not making a request many times
 //like every time a volume changes 1-50--> to make 1 req instead of 50 reqs)
@@ -15,7 +16,7 @@ function Player(){
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     const spotifyApi = useSpotify();
     const {data:session,status} = useSession();
-    const [volume,setVolume] = useState(50);
+    const [volume,setVolume] = useState(70);
 
     const songInfo = useSongInfo();
 
@@ -46,9 +47,23 @@ function Player(){
     useEffect(()=>{
         if(spotifyApi.getAccessToken() && !currentTrackId){
             fetchCurrentSong();
-            setVolume(50);
+            setVolume(70);
         }
     },[currentTrackIdState,spotifyApi,session])
+
+    useEffect(()=>{
+        if(volume>0 && volume<100){
+            debouncedAdjustVolume(volume);
+        }
+    },[volume])
+
+    //debouncing the usecallback for volume change
+    const debouncedAdjustVolume = useCallback(
+        debounce((volume)=>{
+            spotifyApi.setVolume(volume).catch(err => console.log(err));
+        },300),
+        []
+    )
 
     return(
         <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-sm px-2 md:px-8">
@@ -59,6 +74,7 @@ function Player(){
                     <h3>{songInfo?.name}</h3>
                     <p>{songInfo?.artists?.[0]?.name}</p>
                 </div>
+                <HeartIcon className="button"/>
             </div>
             {/* center */}
             <div className="flex items-center justify-evenly">
@@ -70,9 +86,10 @@ function Player(){
             </div>
             {/* right */}
             <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
-                <VolumeDownIcon className="button"/>
+                <MicrophoneIcon className="button"/>
+                <VolumeDownIcon onClick={() => volume>0 && setVolume(volume-10)} className="button"/>
                 <input className="w-16 md:w-28" type="range" value={volume} onChange={(e)=>setVolume(Number(e.target.value))} min={0} max={100}/>
-                <VolumeUpIcon className="button"/>
+                <VolumeUpIcon className="button" onClick={() => volume<100 && setVolume(volume+10)}/>
             </div>
         </div>
     )
